ca9654c5568546c6dc4ff089a90e752221d215ec
RAI\sangitashres <rai.saugat.sr@gmail.com>
2017-02-25 11:24:39 +0545

    Fourth

--- js/gender.js
+++ js/gender.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
      $(".ribbon").on("click", function(){
+          $("#chart-data-second").css("display","none");
           var data = [
         {
             "hc-key": "np-750",
@@ -148,7 +149,7 @@ $(document).ready(function(){
             enabled: true,
             style: {
                fontSize: '18px',
-                color: (Highcharts.theme && Highcharts.theme.textColor) || 'white'
+                color: (Highcharts.theme && Highcharts.theme.textColor) || '#333 '
             }
         }
     },
@@ -173,8 +174,8 @@ $(document).ready(function(){
             dataLabels: {
                 enabled: true,
                 color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
-            }
-        }
+           },
+       },
     },
     series: [{
         name: 'Male',
@@ -516,5 +517,57 @@ series: [{
         stack: 'transgender'
     }]
 });
+     });
+
+     Highcharts.chart('chart-data-third', {
+         chart: {
+             type: 'column'
+         },
+         title: {
+             text: 'Treatment Cascade'
+         },
+         xAxis: {
+             categories: ['Esimated Number of PLHIV', 'Number of Alive PLHIV', 'Linked To HIV care', 'PLHIV Received ART']
+         },
+         yAxis: {
+             min: 0,
+             title: {
+                 text: 'Number Of People'
+             },
+             stackLabels: {
+                 enabled: true,
+                 style: {
+                     fontWeight: 'bold',
+                     color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                 }
+             }
+         },
+         legend: {
+             align: 'right',
+             x: -30,
+             verticalAlign: 'top',
+             y: 25,
+             floating: true,
+             backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+             borderColor: '#CCC',
+             borderWidth: 1,
+             shadow: false
+         },
+         tooltip: {
+             headerFormat: '<b>{point.x}</b><br/>',
+         },
+         plotOptions: {
+             column: {
+                 stacking: 'normal',
+                 dataLabels: {
+                     enabled: false,
+                     color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+                 }
+             }
+         },
+         series: [{
+         		name: 'Records',
+             data: [39397,22267, 20307, 11922]
+         }]
      });
 });
